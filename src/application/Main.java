5250e18f52f27284b66f5017676a5c120e1886b1
Iury Lemos <iurylemos10@gmail.com>
2019-05-24 10:39:23 -0300

    Modificado a main, e adicionando o pacote gui com o MainView

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,25 +1,36 @@
 package application;
-	
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
+			//Instanciando o objeto loader do tipo FXML
+			//Isso vai ser importante para manipular a tela antes de carregar
+			//Na instancia passa o caminho da view para ser carregada
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+			//carregando a view
+			Parent parent = loader.load();
+			//Cena principal
+			Scene mainScene = new Scene(parent);
+			//palco da cena, setando a cena como cena principal
+			primaryStage.setScene(mainScene);
+			//Titulo principal
+			primaryStage.setTitle("Sample JavaFX application");
+			//mostrando o palco
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
